71a4749521660c4dcd6aae69eb0474f316355f7f
andreferi3 <andreferi135@gmail.com>
2019-08-04 21:48:47 +0700

    new repos

---
+++ src/Deck.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react'
+import { 
+    View, 
+    Animated,
+    PanResponder 
+} from 'react-native'
+
+class Deck extends Component {
+
+    constructor(props) {
+        super(props)
+
+        const position = new Animated.ValueXY();
+        const panResponder = PanResponder.create({
+            onStartShouldSetPanResponder: () => true,
+            onPanResponderMove: (event, gesture) => {
+                position.setValue({ x: gesture.dx , y: gesture.dy })
+            },
+            onPanResponderRelease: () => { panResponder, position }
+        })
+
+        this.state = { 
+            panResponder,
+            position
+        }
+    }
+
+    renderCards() {
+        return this.props.data.map((item, i) => {
+            if(i === 0) {
+                return (
+                    <Animated.View
+                        style={this.state.position.getLayout()}
+                        {...this.state.panResponder.panHandlers}
+                        key={i}
+                    >
+                        {this.props.renderCard(item)}
+                    </Animated.View>
+                )
+            }
+
+            return this.props.renderCard(item)
+        })
+    }
+
+    render() {     
+        return (
+            <Animated.View>
+                {this.renderCards()}
+            </Animated.View>
+        )
+    }
+}
+
+export default Deck
